393b10b0515ce222bef58113db8fb999787a7194
amartseniuk <amartseniuk@zeushq.com>
2020-09-30 21:24:18 +0300

    Implemented NetworkService

---
+++ DummyApp/NetworkService.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkService.swift
+//  DummyApp
+//
+//  Created by Anton Martsenyuk on 30.09.2020.
+//  Copyright © 2020 Anton Martsenyuk. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkService {
+    private let alamofire = AF
+}
+
+extension NetworkService {
+    
+    func userListRequest(page: Int, limit: Int,
+                         completion: @escaping (Result<[User], AFError>) -> Void) {
+        let route: UserRouter = .fecthUsersList(page: page, limit: limit)
+        
+        alamofire.request(route)
+            .validate()
+            .responseDecodable(of: UserResponse.self) { result in
+                switch result.result {
+                case .success(let userResponse): completion(.success(userResponse.data))
+                case .failure(let error): completion(.failure(error))
+                }
+        }
+    }
+    
+    func userDetail(with id: String, completion: @escaping (Result<UserProfile, AFError>) -> Void) {
+        let route: UserRouter = .userDetail(id: id)
+        
+        alamofire.request(route)
+            .validate()
+            .responseDecodable(of: UserProfile.self) { result in
+                switch result.result {
+                case .success(let profile): completion(.success(profile))
+                case .failure(let error): completion(.failure(error))
+                }
+        }
+    }
+}
