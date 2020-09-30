3bc6ac04d760a2c352653d86b50ba3e1e036ffe2
amartseniuk <amartseniuk@zeushq.com>
2020-09-30 22:45:02 +0300

    Provided User cell for tableView as xib

---
+++ DummyApp/UI/Cell/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  DummyApp
+//
+//  Created by Anton Martsenyuk on 30.09.2020.
+//  Copyright © 2020 Anton Martsenyuk. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
+
+extension UserCell {
+    static func register(to tableView: UITableView) {
+        let cellString = String(describing: self)
+        let cellNib = UINib(nibName: cellString, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: cellString)
+    }
+}
