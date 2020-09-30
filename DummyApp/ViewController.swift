da993c4aa77b6fd8148ed348aa223d94f4785761
amartseniuk <amartseniuk@zeushq.com>
2020-09-30 16:08:35 +0300

    Initial Commit

---
+++ DummyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DummyApp
+//
+//  Created by Anton Martsenyuk on 30.09.2020.
+//  Copyright © 2020 Anton Martsenyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
